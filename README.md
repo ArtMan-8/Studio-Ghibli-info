4fc3de9b8b2371d1c9633c554e853161caa3947f
Art <ya.Artriches@ya.ru>
2020-05-30 18:01:40 +0300

    webpack - базовая настройка

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Studio-Ghibli
+# Studio Ghibli, info
+
+## Description
+This repository was created for practice.
+
+### API created by [janaipakos](https://github.com/janaipakos/ghibliapi)
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+  entry: { main: './src/main.js' },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: './js/main.js'
+  },
+  devtool: `source-map`,
+  devServer: {
+    contentBase: path.resolve(__dirname, `./public`),
+    watchContentBase: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use:  ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/style.css',
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ]
+};
