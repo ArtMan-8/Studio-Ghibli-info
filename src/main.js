d317dbda76c1bfe951da3b894efa70ab97431cc2
Art <ya.Artriches@ya.ru>
2020-06-02 19:54:54 +0300

    add: функцию рендера, и рендера разметки index

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 import './sass/style.scss';
+
+import createMarkupHeader from './js/components/header';
+import createMarkupFooter from './js/components/footer';
+import createMarkupAbout from './js/components/about';
+
+const EntryPoint = {
+  HEADER: document.querySelector('.header'),
+  MAIN: document.querySelector('.main'),
+  FOOTER: document.querySelector('.footer'),
+};
+
+const renderComponent = (container, markup, place = 'beforeend') => {
+  container.insertAdjacentHTML(place, markup);
+};
+
+renderComponent(EntryPoint.HEADER, createMarkupHeader());
+renderComponent(EntryPoint.FOOTER, createMarkupFooter());
+renderComponent(EntryPoint.MAIN, createMarkupAbout());
