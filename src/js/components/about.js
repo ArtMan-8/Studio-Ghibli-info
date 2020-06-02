47a2f46af8837b630c6c12c079260338bf9c83cd
Art <ya.Artriches@ya.ru>
2020-06-02 19:33:45 +0300

    add: перемещение разметки в компонент about

---
+++ src/js/components/about.js
@@ -0,0 +1,17 @@
+const createMarkupAbout = () => {
+  const markup = (
+    `<section class="about">
+      <h1 class="about__header-h1">Studio Ghibli</h1>
+      <p class="about__text">Studio Ghibli, Inc. (/ˈdʒɪbli/) (Japanese: 株式会社スタジオジブリ, Hepburn: Kabushiki gaisha Sutajio Jiburi) is a Japanese animation film studio based in Koganei, Tokyo, Japan. The studio is best known for its animated feature films, and has also produced several short films, television commercials, and one television film. It was founded on 15 June 1985, after the success of Nausicaä of the Valley of the Wind (1984), with funding by Tokuma Shoten. Studio Ghibli has also collaborated with video game studios on the visual development of several video games.</p>
+      <p class="about__text">Six of Studio Ghibli's films are among the 10 highest-grossing anime films made in Japan, with Spirited Away (2001) being the highest, grossing over US$360 million worldwide. Many of their works have won the Animage Anime Grand Prix award, and four have won the Japan Academy Prize for Animation of the Year. Five of Studio Ghibli's films have received Academy Award nominations. Spirited Away won the Golden Bear in 2002 and the Academy Award for Best Animated Feature Film in 2003. Totoro, a character from My Neighbor Totoro, is the studio's mascot.</p>
+      <p class="about__text">On 3 August 2014, Studio Ghibli temporarily halted production following the retirement of director Hayao Miyazaki, who co-founded the studio with the late Isao Takahata. In February 2017, Toshio Suzuki announced that Miyazaki had come out of retirement again to direct a new feature film, How Do You Live?, with Studio Ghibli.</p>
+
+      <h2 class="about__header-h2">Name</h2>
+      <p class="about__text">The name Ghibli was given by Hayao Miyazaki from the Italian noun ghibli, based on the Libyan-Arabic name for the hot desert wind of that country, the idea being the studio would "blow a new wind through the anime industry". It also refers to an Italian aircraft, the Caproni Ca.309 Ghibli. Although the Italian word is more accurately transliterated as ギブリ (Giburi), the Japanese name of the studio is ジブリ (Jiburi).</p>
+    </section>`
+  );
+
+  return markup;
+};
+
+export default createMarkupAbout;
