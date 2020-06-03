e929da76ec8148cd32b2b54b203d977988a3e445
Art <ya.Artriches@ya.ru>
2020-06-03 19:09:56 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,23 +1,23 @@
 import './sass/style.scss';
 
-import createMarkupHeader from './js/components/header';
-import createMarkupFooter from './js/components/footer';
-import createMarkupAbout from './js/components/about';
+// import createMarkupHeader from './js/components/header';
+// import createMarkupFooter from './js/components/footer';
+// import createMarkupAbout from './js/components/about';
 // import createMarkupFilms from './js/components/films';
 // import createMarkupPeoples from './js/components/peoples';
 
-const EntryPoint = {
-  HEADER: document.querySelector('.header'),
-  MAIN: document.querySelector('.main'),
-  FOOTER: document.querySelector('.footer'),
-};
+// const EntryPoint = {
+//   HEADER: document.querySelector('.header'),
+//   MAIN: document.querySelector('.main'),
+//   FOOTER: document.querySelector('.footer'),
+// };
 
-const renderComponent = (container, markup, place = 'beforeend') => {
-  container.insertAdjacentHTML(place, markup);
-};
+// const renderComponent = (container, markup, place = 'beforeend') => {
+//   container.insertAdjacentHTML(place, markup);
+// };
 
-renderComponent(EntryPoint.HEADER, createMarkupHeader());
-renderComponent(EntryPoint.FOOTER, createMarkupFooter());
+// renderComponent(EntryPoint.HEADER, createMarkupHeader());
+// renderComponent(EntryPoint.FOOTER, createMarkupFooter());
 // renderComponent(EntryPoint.MAIN, createMarkupPeoples());
 // renderComponent(EntryPoint.MAIN, createMarkupFilms());
-renderComponent(EntryPoint.MAIN, createMarkupAbout());
+// renderComponent(EntryPoint.MAIN, createMarkupAbout());
