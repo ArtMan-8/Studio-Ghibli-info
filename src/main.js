00774d807870a02ce226393f2528a146e60d6300
Art <ya.Artriches@ya.ru>
2020-06-06 18:43:07 +0300

    refactoring: переписывает vehicles на класс

--- src/main.js
+++ src/main.js
@@ -7,12 +7,12 @@ import renderComponent from './js/helpers/renderComponent';
 
 import Header from './js/components/header';
 import Footer from './js/components/footer';
-// import About from './js/components/about';
+import About from './js/components/about';
 // import Films from './js/components/films';
 // import Peoples from './js/components/peoples';
 // import Locations from './js/components/locations';
-import Species from './js/components/species';
-// import createMarkupVehicles from './js/components/vehicles';
+// import Species from './js/components/species';
+// import Vehicles from './js/components/vehicles';
 
 const END_POINT = 'https://ghibliapi.herokuapp.com/';
 
@@ -36,16 +36,18 @@ api.getData()
 
 const header = new Header();
 const footer = new Footer();
-// const about = new About();
+const about = new About();
 // const films = new Films();
 // const peoples = new Peoples();
 // const locations = new Locations();
-const species = new Species();
+// const species = new Species();
+// const vehicles = new Vehicles();
 
 renderComponent(EntryPoint.HEADER, header.getElement());
 renderComponent(EntryPoint.FOOTER, footer.getElement());
-// renderComponent(EntryPoint.MAIN, about.getElement());
+renderComponent(EntryPoint.MAIN, about.getElement());
 // renderComponent(EntryPoint.MAIN, films.getElement());
 // renderComponent(EntryPoint.MAIN, peoples.getElement());
 // renderComponent(EntryPoint.MAIN, locations.getElement());
-renderComponent(EntryPoint.MAIN, species.getElement());
+// renderComponent(EntryPoint.MAIN, species.getElement());
+// renderComponent(EntryPoint.MAIN, vehicles.getElement());
