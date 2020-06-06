808bc155e557e9b04a90722947e805b91026473d
Art <ya.Artriches@ya.ru>
2020-06-06 16:58:41 +0300

    add: перемещение разметки в компонент species

---
+++ src/js/components/species.js
@@ -0,0 +1,129 @@
+const createMarkupSpecies = () => {
+  const markup = (
+    `<section class="species">
+      <article class="species__item specie">
+        <h1 class="specie__header">Spirit</h1>
+
+        <ul class="specie__info">
+          <li>classification: <b>Spirit</b></li>
+          <li>eye colors: <b>Red</b></li>
+          <li>hair colors: <b>Light Orange</b></li>
+        </ul>
+
+        <h2 class="specie__subheader">peoples:</h2>
+        <ul class="specie__peoples">
+          <li><a href="">- Shishigami</a></li>
+        </ul>
+
+        <h2 class="specie__subheader">films:</h2>
+        <ul class="specie__films">
+          <li><a href="">- Princess Mononoke</a></li>
+        </ul>
+      </article>
+
+      <article class="species__item specie">
+        <h1 class="specie__header">Spirit</h1>
+
+        <ul class="specie__info">
+          <li>classification: <b>Spirit</b></li>
+          <li>eye colors: <b>Red</b></li>
+          <li>hair colors: <b>Light Orange</b></li>
+        </ul>
+
+        <h2 class="specie__subheader">peoples:</h2>
+        <ul class="specie__peoples">
+          <li><a href="">- Shishigami</a></li>
+        </ul>
+
+        <h2 class="specie__subheader">films:</h2>
+        <ul class="specie__films">
+          <li><a href="">- Princess Mononoke</a></li>
+        </ul>
+      </article>
+
+      <article class="species__item specie">
+        <h1 class="specie__header">Spirit</h1>
+
+        <ul class="specie__info">
+          <li>classification: <b>Spirit</b></li>
+          <li>eye colors: <b>Red</b></li>
+          <li>hair colors: <b>Light Orange</b></li>
+        </ul>
+
+        <h2 class="specie__subheader">peoples:</h2>
+        <ul class="specie__peoples">
+          <li><a href="">- Shishigami</a></li>
+        </ul>
+
+        <h2 class="specie__subheader">films:</h2>
+        <ul class="specie__films">
+          <li><a href="">- Princess Mononoke</a></li>
+        </ul>
+      </article>
+
+      <article class="species__item specie">
+        <h1 class="specie__header">Spirit</h1>
+
+        <ul class="specie__info">
+          <li>classification: <b>Spirit</b></li>
+          <li>eye colors: <b>Red</b></li>
+          <li>hair colors: <b>Light Orange</b></li>
+        </ul>
+
+        <h2 class="specie__subheader">peoples:</h2>
+        <ul class="specie__peoples">
+          <li><a href="">- Shishigami</a></li>
+        </ul>
+
+        <h2 class="specie__subheader">films:</h2>
+        <ul class="specie__films">
+          <li><a href="">- Princess Mononoke</a></li>
+        </ul>
+      </article>
+
+      <article class="species__item specie">
+        <h1 class="specie__header">Spirit</h1>
+
+        <ul class="specie__info">
+          <li>classification: <b>Spirit</b></li>
+          <li>eye colors: <b>Red</b></li>
+          <li>hair colors: <b>Light Orange</b></li>
+        </ul>
+
+        <h2 class="specie__subheader">peoples:</h2>
+        <ul class="specie__peoples">
+          <li><a href="">- Shishigami</a></li>
+        </ul>
+
+        <h2 class="specie__subheader">films:</h2>
+        <ul class="specie__films">
+          <li><a href="">- Princess Mononoke</a></li>
+        </ul>
+      </article>
+
+      <article class="species__item specie">
+        <h1 class="specie__header">Spirit</h1>
+
+        <ul class="specie__info">
+          <li>classification: <b>Spirit</b></li>
+          <li>eye colors: <b>Red</b></li>
+          <li>hair colors: <b>Light Orange</b></li>
+        </ul>
+
+        <h2 class="specie__subheader">peoples:</h2>
+        <ul class="specie__peoples">
+          <li><a href="">- Shishigami</a></li>
+        </ul>
+
+        <h2 class="specie__subheader">films:</h2>
+        <ul class="specie__films">
+          <li><a href="">- Princess Mononoke</a></li>
+        </ul>
+      </article>
+    </section>`
+  );
+
+  return markup;
+};
+
+export default createMarkupSpecies;
